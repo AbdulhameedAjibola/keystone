cf50c313bc70d875e1985dc87ec79c8dfe82a309
AbdulhameedAjibola <akoredeajibola091@gmail.com>
2025-12-31 17:34:49 +0100

    modified:   config/cors.php

--- config/cors.php
+++ config/cors.php
@@ -22,7 +22,7 @@ return [
     'allowed_origins' => [
         'http://localhost:3000', 
         'http://localhost:5173',
-        'https://keystone-realestate.vercel.app/'
+        'https://keystone-realestate.vercel.app'
     ],
 
     'allowed_origins_patterns' => [],
