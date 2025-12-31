ba7004edee359e18ba5c29c3c4eab6f7f76995e1
AbdulhameedAjibola <akoredeajibola091@gmail.com>
2025-12-31 14:21:57 +0100

    modified:   app/Providers/AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,12 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Models\Property;
-use App\Models\Inquiry;
-use App\Models\Agent;
-use App\Policies\PropertyPolicy;
-use App\Policies\InquiryPolicy;
-use App\Policies\AgentPolicy;
+
+use Cloudinary\Configuration\Configuration;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -23,14 +22,47 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
+     *
+     * This method is called after all the other service providers have been registered,
+     * but before any bootstrapping has taken place. Here we can configure the Cloudinary
+     * library and set up rate limiting for API requests.
+     *
+     * @return void
      */
     public function boot(): void
     {
-        $policies = [
-        Property::class => PropertyPolicy::class,
-        Inquiry::class  => InquiryPolicy::class,
-        Agent::class    => AgentPolicy::class,
-    ];
+        // Configure the Cloudinary library
+      
+       
+     Configuration::instance([
+            'cloud' => [
+                'cloud_name' => config('cloudinary.cloud_name'),
+                'api_key'    => config('cloudinary.api_key'),
+                'api_secret' => config('cloudinary.api_secret'),
+            ],
+            'url' => [
+                'secure' => true
+            ]
+        ]);
+
+        // Set up rate limiting for API requests
+        // This will limit API requests to 60 per minute per user or IP address
+        RateLimiter::for('api', function(Request $request) {
+            return Limit::perMinute(40)->by($request->user()?->id ?: $request->ip());
+        });
+
+        RateLimiter::for('guest', function(Request $request) {
+            return Limit::perMinute(15)->by($request->ip());
+        });
+
+        RateLimiter::for('public-properties', function (Request $request) {
+            return [
+                Limit::perMinute(80)->by($request->ip()),     
+                Limit::perMinute(15)->by('search:' . $request->ip()), 
+            ];
+        });
+
 
     }
+
 }
